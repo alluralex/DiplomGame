0ca28ce4e653087c2f857548abc25a496acf3e63
alluralex <alluralexk@gmail.com>
2025-10-06 00:57:31 +1000

    Создание новой ветви
    Сделал поворот персонажа + перенёс добычу предметов под новую input system, но до сих пор есть баг, что если добыть объект, то персонаж якобы будет продолжать его далее добывать пока не сменит объект для добычи

---
+++ Assets/Scripts/Hero/GoingToCar.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GoingToCar : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
