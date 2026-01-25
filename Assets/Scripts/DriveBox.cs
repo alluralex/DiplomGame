da57d5cdaddac902f2d9d23ceee28c0478924599
alluralex <alluralexk@gmail.com>
2026-01-25 17:41:56 +1000

    Adding New Car Movement
    Driving in my truck right after a beer
    Hey, that bump is shaped like a deer
    DUI?
    How about you die?
    I'll go a hundred miles
    An hour!
    
    Little do you know, I filled up on gas
    Imma get your fountain-making ass
    Pulverize this fuck
    ​With my Bergentrück
    It seems you're out of luck!
    TRUCK!

--- Assets/Scripts/DriveBox.cs
+++ Assets/Scripts/DriveBox.cs
@@ -2,17 +2,24 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SocialPlatforms;
 
 public class DriveBox : MonoBehaviour
 {
+    [SerializeField] private WheelCollider LeftBackWheel;
+    [SerializeField] private WheelCollider RightBackWheel;
+    [SerializeField] private WheelCollider RightFrontWheel;
+    [SerializeField] private WheelCollider LeftFrontWheel;
+
     [SerializeField] private float _speedMove;
     [SerializeField] private float _speedRotate;
     [SerializeField] Hero driver;
-    [SerializeField] List<Hero> passengers;
     [SerializeField] Camera cameraCar;
 
     public Vector3 rawInputMovement;
     public Vector3 rawInputRotation;
+
+    float HorizontalInput, VerticalInput;
 
     public void TryGoCar(Hero player)
     {
@@ -21,42 +28,55 @@ public class DriveBox : MonoBehaviour
             driver = player;
             //player.gameObject.SetActive(false);
         }
-        else if (driver != null && passengers.Count <= 3)
-        {
-            passengers.Add(player);
-            //player.gameObject.SetActive(false);
-        }
         else
         {
             Debug.Log("Тачка забита до отказа...");
         }
     }
 
+    private void Update()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Horizontal");
+    }
+
     private void FixedUpdate()
     {
         if (driver != null)
         {
-            //if (rawInputRotation.y != 0)
-            //{
-            //}
-            //if (rawInputMovement.x != 0 || rawInputMovement.z != 0)
-            //{
-            //}
-            transform.Rotate(rawInputRotation * _speedRotate * Time.deltaTime);
-            transform.position += transform.TransformDirection(rawInputMovement) * _speedMove;
+            float motor = Input.GetAxis("Vertical") * _speedMove;
+            LeftBackWheel.motorTorque = motor;
+            RightBackWheel.motorTorque = motor;
+            RightFrontWheel.motorTorque = motor;
+            LeftFrontWheel.motorTorque = motor;
+
+            RightFrontWheel.steerAngle = _speedRotate * HorizontalInput;
+            LeftFrontWheel.steerAngle = _speedRotate * HorizontalInput;
+
+            //float GoCar = rawInputMovement.x * _speedMove;
+            //float RotateCar = rawInputRotation.y * _speedRotate;
+
+            //LeftBackWheel.motorTorque += GoCar;
+            //RightBackWheel.motorTorque += GoCar;
+            //RightFrontWheel.motorTorque += GoCar;
+            //LeftFrontWheel.motorTorque += GoCar;
+
+            //RightFrontWheel.steerAngle += RotateCar;
+            //LeftFrontWheel.steerAngle += RotateCar;
+
         }
     }
 
-    public void OnRotateCar(InputAction.CallbackContext value)
-    {
-        Vector2 vector2 = value.ReadValue<Vector2>();
-        rawInputRotation = new Vector3(0, vector2.x, 0);
-    }
+    //public void OnRotateCar(InputAction.CallbackContext value)
+    //{
+    //    Vector2 vector2 = value.ReadValue<Vector2>();
+    //    rawInputRotation = new Vector3(0, vector2.x, 0);
+    //}
 
-    public void OnMoveCar(InputAction.CallbackContext value)
-    {
-        Vector2 vector2 = value.ReadValue<Vector2>();
-        rawInputMovement = new Vector3(vector2.x, 0, vector2.y);
-    }
+    //public void OnMoveCar(InputAction.CallbackContext value)
+    //{
+    //    Vector2 vector2 = value.ReadValue<Vector2>();
+    //    rawInputMovement = new Vector3(vector2.x, 0, vector2.y);
+    //}
 
 }
